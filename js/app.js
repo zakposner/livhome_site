538baed4425157ffa6461c3a74f40ad1ef018942
Zach Posner <zposner@BOS-CONFRM-1.livhome.com>
2017-06-02 09:38:10 -0700

    style updates, popup application

--- js/app.js
+++ js/app.js
@@ -1,5 +1,21 @@
 jQuery(document).ready(function() {
 
+  var liv_home_vars = {
+    mobileCutoff: 800 // pixel width trigger point for many functions, chnage this to adjust which devices are impacted by mobile-only functions
+  }
+
+  // Determine the size of the user's device
+  function getWindowWidth() {
+    var width = $(window).width();
+    console.log('Window width set: ', width);
+    return width;
+  }
+
+  function checkForHomePage() {
+    var $body = $('body');
+    return $body.hasClass('home');
+  }
+
   // ==========
   //  Revolution Mobile Config
   // ==========
@@ -12,17 +28,20 @@ jQuery(document).ready(function() {
         // 6. section must have the id 'rev-mobile-slide-[number]'
   // ==========
 
-  // Change this number to alter when mobile config executes
-  var mobileCutoff = 800; // pixel width of viewing device
+  (function() {
+
+  })()
 
   // DOM Referneces
   var $sliderRef = $('#rev_slider_2_1'),
       $sectionRef = $('.home-page-top-section');
 
+
   // Build an array of the mobile slides
+    // FIXME add <a> to the buttons
   var $mobileSlides = [
-    $('<section id="rev-mobile-slide-1" class="rev-mobile-slide"><div class="rev-primary-mobile"><h2>Life is better with LivHOME</h2><h1>Caregiving + Life Care Management</h1><p>Our Life Care Managers works collaboratively with Caregivers to ease the burden of care—so you can be a family again.</p><div class="button-box"><button type="button" class="cta-primary">1-800-807-5854</button><button type="button" class="cta-secondary">Learn More</button></div></div></section>'),
-    $('<section id="rev-mobile-slide-2" class="rev-mobile-slide"><div class="rev-connect-mobile"><h2>Introducing LivHOME Connect</h2><p>LivHOME Connect is a senior-friendly care tablet—helping seniors and families  stay informed and in control of their care.</p><button class="cta-primary">Learn More</button></div></section>')
+    $('<section id="rev-mobile-slide-1" class="rev-mobile-slide"><div class="rev-primary-mobile"><h2>Life is better with LivHOME</h2><h1>Caregiving + Life Care Management</h1><p>Our Life Care Managers work collaboratively with Caregivers to ease the burden of care—so you can be a family again.</p><div class="button-box"><button type="button" class="cta-primary">1-800-807-5854</button><button type="button" class="cta-secondary">Learn More</button></div></div></section>'),
+    $('<section id="rev-mobile-slide-2" class="rev-mobile-slide"><div class="rev-connect-mobile"><h2>Introducing LivHOME Connect</h2><p>LivHOME Connect is a senior-friendly care tablet—helping seniors and families stay informed and in control of their care.</p><button class="cta-primary">Learn More</button></div></section>')
   ]
 
   // Attach the mobile slides, then hide them
@@ -31,13 +50,6 @@ jQuery(document).ready(function() {
   });
   $boundSlides = $('.rev-mobile-slide');
   $boundSlides.hide();
-
-  // Determine the size of the user's device
-  function getWindowWidth() {
-    var width = $(window).width();
-    console.log('Window width set: ', width);
-    return width;
-  }
 
   // renders the text to the corresponding slideIndex
     // binds to revolution slider event
@@ -53,12 +65,38 @@ jQuery(document).ready(function() {
     // otherwise leave everything as is
   function configRevolution() {
     var width = getWindowWidth();
-    if (width < mobileCutoff) {
+    var onHomePage = checkForHomePage();
+    if (onHomePage && width < liv_home_vars.mobileCutoff) {
         revapi2.bind('revolution.slide.onchange', initMobileSlide);
     }
   }
 
-  configRevolution();
+  window.addEventListener('load', configRevolution);
+
+
+
+  // ==========
+  //  Mobile Caregiver Application
+  // ==========
+
+  function initCaregiverApp() {
+    var width = getWindowWidth();
+    var onHomePage = checkForHomePage();
+    if (onHomePage && width < liv_home_vars.mobileCutoff) { // if we're on a mobile device on the home page
+      // build the caregiver application popup
+      var caregiverApp = $('<div class="caregiver-application"><a class="sign-up" href="#"><button class="cta-primary" type="button" name="button">Apply to be a caregiver</button></a><div class="close-app"><i class="fa fa-times" aria-hidden="true"></i></div></div>');
+
+      // Attach the popup
+      $('.l-canvas').before(caregiverApp);
+
+      //bind close function to .close-app click
+      $('.close-app').click(function(e) {
+        $('.caregiver-application').slideUp();
+      })
+    }
+  }
+
+  window.addEventListener('load', initCaregiverApp);
 
   // ==========
   //  Mini Form Config
@@ -68,14 +106,14 @@ jQuery(document).ready(function() {
       $miniSelect = $('.looking-select'),
       $miniSubmit = $('.looking-form .cta-primary');
 
-  // Enable submit button if a valid option is selected
+  // Only enable submit button if a valid option is selected
   $miniSelect.change(function(e) {
     var currentVal = $miniSelect.val();
-    console.log('current value: ', currentVal);
+
     if (currentVal !== 'null') {
-      $miniSubmit.disabled = false;
+      $miniSubmit.prop('disabled', false);
     } else {
-      $miniSubmit.disabled = true;
+      $miniSubmit.prop('disabled', true);
     }
   });
 
@@ -84,6 +122,8 @@ jQuery(document).ready(function() {
     e.preventDefault(); // stop browser refresh
     var carefor = $('.looking-select').val(); // pull desired value from $miniform
     carefor = encodeURIComponent(carefor); // encode value for query string
+
+    // FIXME change url at site launch
     window.location.href= 'http://bwmsites.com/livhome2/contact?carefor=' + carefor; // send data to contact form
   });
 
